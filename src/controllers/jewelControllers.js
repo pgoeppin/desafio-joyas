082e22816e8093dfb60b18b613039416eb46f529
Pablo Goeppinger <pgoeppin@gmail.com>
2023-01-12 17:35:35 -0300

    Adding HATEOAS

--- src/controllers/jewelControllers.js
+++ src/controllers/jewelControllers.js
@@ -1,13 +1,16 @@
 const {
-    getJewels
-} = require("../models/jewelModels")
+    getJewels,
+    prepareHATEOAS,
+} = require("../models/jewelModels");
 
 const getAllJewels = async (req,res) => {
     const limits = req.query.limits
     const order_by = req.query.order_by
+    const page = req.query.page
     try {
-        const jewels = await getJewels({limits: limits, order_by: order_by});
-        res.json(jewels);
+        const jewels = await getJewels({limits: limits, order_by: order_by, page: page});
+        const HATEOAS = await prepareHATEOAS(jewels)
+        res.json(HATEOAS);
     } catch (e) {
         console.log(e);
         res.status(500).json({ message: "Error 500. No se pudo obtener los datos" })
--- src/models/jewelModels.js
+++ src/models/jewelModels.js
@@ -2,12 +2,31 @@ const pool = require("../helpers/connectionDB").getInstance();
 const format = require('pg-format');
 
 
-const getJewels = async ({limits = 4, order_by = "id_ASC"}) => {
+const getJewels = async ({limits = 4, order_by = "id_ASC", page = 1}) => {
     const [field, direction] = order_by.split("_");
-    const formattedQuery = format('SELECT * FROM inventario ORDER BY %s %s LIMIT %s', field, direction, limits);
+    const offset = (page - 1) * limits
+    const formattedQuery = format('SELECT * FROM inventario ORDER BY %s %s LIMIT %s OFFSET %s', field, direction, limits, offset);
     const { rows: jewels } = await pool.query(formattedQuery);
     console.log(jewels);
     return jewels;
 }
 
-module.exports = { getJewels }
+const prepareHATEOAS = (jewels) => {
+
+    const results = jewels.map((jewel) => {
+        return {
+            name: jewel.nombre,
+            href: `/joyas/joya/${jewel.id}`,
+        }
+    }).slice(0, 4)
+    const totalJoyas = jewels.length
+    const stockTotal = jewels.map(jewel => jewel.stock).reduce((a,b) => a + b)
+    const HATEOAS = {
+        totalJoyas,
+        stockTotal,
+        results
+    }
+    return HATEOAS
+}
+
+module.exports = { getJewels, prepareHATEOAS }
