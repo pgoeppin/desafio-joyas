c1d9f9c93827df25347bfae6ac10f4dd665a5631
Pablo Goeppinger <pgoeppin@gmail.com>
2023-01-12 17:18:49 -0300

    Adding parameters to get feature

--- src/controllers/jewelControllers.js
+++ src/controllers/jewelControllers.js
@@ -3,8 +3,10 @@ const {
 } = require("../models/jewelModels")
 
 const getAllJewels = async (req,res) => {
+    const limits = req.query.limits
+    const order_by = req.query.order_by
     try {
-        const jewels = await getJewels();
+        const jewels = await getJewels({limits: limits, order_by: order_by});
         res.json(jewels);
     } catch (e) {
         console.log(e);
--- src/models/jewelModels.js
+++ src/models/jewelModels.js
@@ -1,9 +1,13 @@
 const pool = require("../helpers/connectionDB").getInstance();
+const format = require('pg-format');
 
-const getJewels = async () => {
-    const { rows } = await pool.query('SELECT * FROM inventario');
-    console.log(rows);
-    return rows;
+
+const getJewels = async ({limits = 4, order_by = "id_ASC"}) => {
+    const [field, direction] = order_by.split("_");
+    const formattedQuery = format('SELECT * FROM inventario ORDER BY %s %s LIMIT %s', field, direction, limits);
+    const { rows: jewels } = await pool.query(formattedQuery);
+    console.log(jewels);
+    return jewels;
 }
 
 module.exports = { getJewels }
